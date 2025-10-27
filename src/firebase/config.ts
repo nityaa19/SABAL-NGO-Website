3db038db3ff08f561fdd8792a0aeab72ebfba5e8
NITYANAND KUMAR (RA2311003010954) <nk1706@srmist.edu.in>
2025-10-27 09:16:24 +0000

    changed-code-files -- --all

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3162190353-f206e",
+  "appId": "1:626632136843:web:dbf7339fb816a83d1f1c8e",
+  "apiKey": "AIzaSyBvQaqlNqyehcqM6GkZdkBOSHvyYLbXv98",
+  "authDomain": "studio-3162190353-f206e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "626632136843"
+};
